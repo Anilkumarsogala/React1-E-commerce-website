9369e884b5dc9cd9949ccdc5d07087bd82b0373f
Anilkumar <anilkumarsogala@gmail.com>
2024-04-25 11:12:32 +0530

    Updates

---
+++ precache-manifest.3168389969d3713d57b4d8032af387f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b718aa3cf42f90b5c11",
+    "url": "/React1-E-commerce-website/static/css/main.a2dc3806.chunk.css"
+  },
+  {
+    "revision": "2b718aa3cf42f90b5c11",
+    "url": "/React1-E-commerce-website/static/js/main.2b718aa3.chunk.js"
+  },
+  {
+    "revision": "adb2852fae2b928deb4c",
+    "url": "/React1-E-commerce-website/static/js/1.adb2852f.chunk.js"
+  },
+  {
+    "revision": "4214000fa79040eff8df",
+    "url": "/React1-E-commerce-website/static/js/2.4214000f.chunk.js"
+  },
+  {
+    "revision": "887f512a94e1a5752659",
+    "url": "/React1-E-commerce-website/static/js/runtime~main.887f512a.js"
+  },
+  {
+    "revision": "295e802b5ef045828e40f30e12b566df",
+    "url": "/React1-E-commerce-website/index.html"
+  }
+];
