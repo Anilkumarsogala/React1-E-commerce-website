cdbc1808ead5aa9f94a05f2e10ec3501893bee88
Sogala Anil kumar <118277928+Anilkumarsogala@users.noreply.github.com>
2024-04-28 11:02:26 +0530

    home.js

--- src/home.js
+++ src/home.js
@@ -41,7 +41,7 @@ const Home = ({detail,view,close,setClose,addtocart}) => {
                     <Link to='/product' className='link'>Shop Now<i class="fa-solid fa-arrow-right"></i></Link>
                 </div>
                 <div className='img_box'>
-                    <img src='./img/slider-img.png' alt="sliding"></img>
+                    <img src='/public/img/mobilegoogle.jpg' alt="sliding"></img>
                 </div>
             </div>
         </div>
